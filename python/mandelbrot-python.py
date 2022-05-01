35a333a7b1cc8e7edcd1c01c46f52f5e75187f80
Pete Richardson <pete@peterichardson.com>
2022-04-30 21:46:55 -0400

    Python: flesh out skeleton code.  Still basically useless

--- python/mandelbrot-python.py
+++ python/mandelbrot-python.py
@@ -1,20 +1,24 @@
-from os import lseek
 import png
 
+def pixel_to_point(bounds, pixel, upper_left, lower_right): 
+    return 1 + 0.5j
 
-def render(image):
-    image = image = [[255, 0, 0, 255],
-             [0, 255, 255, 0]]
+def render(image, height, width, upper_left, lower_right):
+    pass
 
 def write_image(filename, image):
     png.from_array(image, 'L').save(filename)
 
 if __name__ == "__main__":
-
+    image = [[255, 0, 0, 255],
+             [0, 255, 255, 0]]
     filename = "mandelbrot-python.png"
-    height = 
+    height = 750
+    width  = 1000
+    upper_left = (-1, 1)
+    lower_right = (1, -1)
+
     render(image, height, width, upper_left, lower_right)
 
     write_image(filename, image)
     
-    print("Hello, Carl!\n")
